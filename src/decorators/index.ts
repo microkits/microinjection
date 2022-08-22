b50573b98211cec4e046db97f8ba0b372bcc7ac8
Guilherme Covre Dalleprane <guilherme.dalleprane@coopeavi.coop.br>
2022-08-22 09:56:16 -0300

    refactor: removes decorators from the main package

--- src/decorators/index.ts
+++
@@ -1,5 +0,0 @@
-export { ContextScoped } from "./ContextScoped";
-export { SingletonScoped } from "./SingletonScoped";
-export { TransientScoped } from "./TransientScoped";
-export { Inject } from "./Inject";
-export { Injectable } from "./Injectable";
