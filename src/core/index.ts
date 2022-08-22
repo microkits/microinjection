1c9bfc725361c7a32969d2369d9c8b0e4a1afda6
Guilherme Covre Dalleprane <guilherme.dalleprane@coopeavi.coop.br>
2022-08-22 12:45:24 -0300

    feat: exports Scope and RegistrationId

--- src/core/index.ts
+++ src/core/index.ts
@@ -3,3 +3,4 @@ export { Container } from "./Container";
 export { Registration } from "./Registration";
 export { Registry } from "./Registry";
 export { ResolutionContext } from "./ResolutionContext";
+export { RegistrationId } from "./Registration.types";
